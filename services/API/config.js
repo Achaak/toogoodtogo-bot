e78f116ae2d557dd69e2bb551b0f73c57cc53f5a
Axel Lavoie <axel.lavoie@gmail.com>
2020-09-28 20:02:25 +0200

    ...

---
+++ services/API/config.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import config from "./../../config"
+
+const api = axios.create({
+  baseURL: 'https://apptoogoodtogo.com/api/',
+  timeout: 1000,
+  headers: {
+    "User-Agent": config.api.headers["User-Agent"],
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "Accept-Language": "en-US",
+  },
+  responseType: "json",
+  resolveBodyOnly: true,
+});
+
+export {
+  api
+}
